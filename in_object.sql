8e7649cac8fab9988e47e5920d8954013b86b453
iskakov <35532454+iskakov@users.noreply.github.com>
2020-07-09 15:08:49 +0500

    Заполнение значениями таблицы
    Таблица Объекты теплопотребления

---
+++ in_object.sql
@@ -0,0 +1,266 @@
+USE [Task3]
+GO
+
+INSERT INTO [dbo].[ObjectHeatConsumption]
+           ([primarykey]
+           ,[name]
+           ,[date_registration]
+           ,[consumer_id]
+           ,[building_id])
+     VALUES
+           (NEWID()
+           ,'人孛事'
+           ,'23.05.2018'
+           ,'6A1B59CD-5331-4EEF-8FD4-21D335D67050'
+           ,'0E8C5F87-0EC2-42B1-A6A1-55B2A05A8277')
+GO
+INSERT INTO [dbo].[ObjectHeatConsumption]
+           ([primarykey]
+           ,[name]
+           ,[date_registration]
+           ,[consumer_id]
+           ,[building_id])
+     VALUES
+           (NEWID()
+           ,'人孛事'
+           ,'18.04.2018'
+           ,'DC8CE1A9-4C3C-4788-894A-572E8955C4FE'
+           ,'F84E2CB4-F719-4110-9DEF-88A696648AD1')
+GO
+INSERT INTO [dbo].[ObjectHeatConsumption]
+           ([primarykey]
+           ,[name]
+           ,[date_registration]
+           ,[consumer_id]
+           ,[building_id])
+     VALUES
+           (NEWID()
+           ,'人孛事'
+           ,'02.10.2020'
+           ,'6A1B59CD-5331-4EEF-8FD4-21D335D67050'
+           ,'54D3CC66-BE30-400F-B252-A31D5C27AE35')
+GO
+INSERT INTO [dbo].[ObjectHeatConsumption]
+           ([primarykey]
+           ,[name]
+           ,[date_registration]
+           ,[consumer_id]
+           ,[building_id])
+     VALUES
+           (NEWID()
+           ,'人孛事'
+           ,'10.09.2020'
+           ,'6A1B59CD-5331-4EEF-8FD4-21D335D67050'
+           ,'FE1C82E5-5A2D-44E6-A790-A988AC1F3404')
+GO
+INSERT INTO [dbo].[ObjectHeatConsumption]
+           ([primarykey]
+           ,[name]
+           ,[date_registration]
+           ,[consumer_id]
+           ,[building_id])
+     VALUES
+           (NEWID()
+           ,'人孛事'
+           ,'11.08.2019'
+           ,'DC8CE1A9-4C3C-4788-894A-572E8955C4FE'
+           ,'1F3F8C65-04F9-45D9-B259-87852A067FAB')
+GO
+INSERT INTO [dbo].[ObjectHeatConsumption]
+           ([primarykey]
+           ,[name]
+           ,[date_registration]
+           ,[consumer_id]
+           ,[building_id])
+     VALUES
+           (NEWID()
+           ,'人孛事'
+           ,'03.11.2019'
+           ,'D4F0CC52-224D-4035-A39A-23517E92DCD1'
+           ,'1F3F8C65-04F9-45D9-B259-87852A067FAB')
+GO
+INSERT INTO [dbo].[ObjectHeatConsumption]
+           ([primarykey]
+           ,[name]
+           ,[date_registration]
+           ,[consumer_id]
+           ,[building_id])
+     VALUES
+           (NEWID()
+           ,'人孛事'
+           ,'18.03.2019'
+           ,'12663B9A-9543-4726-B904-71F70FB4EB8D'
+           ,'B49A370C-E944-4363-AFB7-B8B563260D16')
+GO
+INSERT INTO [dbo].[ObjectHeatConsumption]
+           ([primarykey]
+           ,[name]
+           ,[date_registration]
+           ,[consumer_id]
+           ,[building_id])
+     VALUES
+           (NEWID()
+           ,'人孛事'
+           ,'16.08.2020'
+           ,'12663B9A-9543-4726-B904-71F70FB4EB8D'
+           ,'B49A370C-E944-4363-AFB7-B8B563260D16')
+GO
+INSERT INTO [dbo].[ObjectHeatConsumption]
+           ([primarykey]
+           ,[name]
+           ,[date_registration]
+           ,[consumer_id]
+           ,[building_id])
+     VALUES
+           (NEWID()
+           ,'人孛事'
+           ,'27.01.2019'
+           ,'12663B9A-9543-4726-B904-71F70FB4EB8D'
+           ,'F84E2CB4-F719-4110-9DEF-88A696648AD1')
+GO
+INSERT INTO [dbo].[ObjectHeatConsumption]
+           ([primarykey]
+           ,[name]
+           ,[date_registration]
+           ,[consumer_id]
+           ,[building_id])
+     VALUES
+           (NEWID()
+           ,'人孛事'
+           ,'23.11.2019'
+           ,'D4F0CC52-224D-4035-A39A-23517E92DCD1'
+           ,'6D05DFAD-BD67-48F8-A9CA-2E507CC0496D')
+GO
+INSERT INTO [dbo].[ObjectHeatConsumption]
+           ([primarykey]
+           ,[name]
+           ,[date_registration]
+           ,[consumer_id]
+           ,[building_id])
+     VALUES
+           (NEWID()
+           ,'人孛事'
+           ,'26.08.2018'
+           ,'12663B9A-9543-4726-B904-71F70FB4EB8D'
+           ,'0E8C5F87-0EC2-42B1-A6A1-55B2A05A8277')
+GO
+INSERT INTO [dbo].[ObjectHeatConsumption]
+           ([primarykey]
+           ,[name]
+           ,[date_registration]
+           ,[consumer_id]
+           ,[building_id])
+     VALUES
+           (NEWID()
+           ,'人孛事'
+           ,'01.01.2020'
+           ,'12663B9A-9543-4726-B904-71F70FB4EB8D'
+           ,'FE1C82E5-5A2D-44E6-A790-A988AC1F3404')
+GO
+INSERT INTO [dbo].[ObjectHeatConsumption]
+           ([primarykey]
+           ,[name]
+           ,[date_registration]
+           ,[consumer_id]
+           ,[building_id])
+     VALUES
+           (NEWID()
+           ,'人孛事'
+           ,'06.03.2018'
+           ,'D4F0CC52-224D-4035-A39A-23517E92DCD1'
+           ,'B49A370C-E944-4363-AFB7-B8B563260D16')
+GO
+INSERT INTO [dbo].[ObjectHeatConsumption]
+           ([primarykey]
+           ,[name]
+           ,[date_registration]
+           ,[consumer_id]
+           ,[building_id])
+     VALUES
+           (NEWID()
+           ,'人孛事'
+           ,'13.03.2019'
+           ,'6A1B59CD-5331-4EEF-8FD4-21D335D67050'
+           ,'FE1C82E5-5A2D-44E6-A790-A988AC1F3404')
+GO
+INSERT INTO [dbo].[ObjectHeatConsumption]
+           ([primarykey]
+           ,[name]
+           ,[date_registration]
+           ,[consumer_id]
+           ,[building_id])
+     VALUES
+           (NEWID()
+           ,'人孛事'
+           ,'22.02.2020'
+           ,'6A1B59CD-5331-4EEF-8FD4-21D335D67050'
+           ,'0E8C5F87-0EC2-42B1-A6A1-55B2A05A8277')
+GO
+INSERT INTO [dbo].[ObjectHeatConsumption]
+           ([primarykey]
+           ,[name]
+           ,[date_registration]
+           ,[consumer_id]
+           ,[building_id])
+     VALUES
+           (NEWID()
+           ,'人孛事'
+           ,'21.08.2019'
+           ,'D4F0CC52-224D-4035-A39A-23517E92DCD1'
+           ,'B49A370C-E944-4363-AFB7-B8B563260D16')
+GO
+INSERT INTO [dbo].[ObjectHeatConsumption]
+           ([primarykey]
+           ,[name]
+           ,[date_registration]
+           ,[consumer_id]
+           ,[building_id])
+     VALUES
+           (NEWID()
+           ,'人孛事'
+           ,'21.02.2020'
+           ,'D4F0CC52-224D-4035-A39A-23517E92DCD1'
+           ,'6D05DFAD-BD67-48F8-A9CA-2E507CC0496D')
+GO
+INSERT INTO [dbo].[ObjectHeatConsumption]
+           ([primarykey]
+           ,[name]
+           ,[date_registration]
+           ,[consumer_id]
+           ,[building_id])
+     VALUES
+           (NEWID()
+           ,'人孛事'
+           ,'03.04.2020'
+           ,'D4F0CC52-224D-4035-A39A-23517E92DCD1'
+           ,'0E8C5F87-0EC2-42B1-A6A1-55B2A05A8277')
+GO
+INSERT INTO [dbo].[ObjectHeatConsumption]
+           ([primarykey]
+           ,[name]
+           ,[date_registration]
+           ,[consumer_id]
+           ,[building_id])
+     VALUES
+           (NEWID()
+           ,'人孛事'
+           ,'25.08.2020'
+           ,'DC8CE1A9-4C3C-4788-894A-572E8955C4FE'
+           ,'FE1C82E5-5A2D-44E6-A790-A988AC1F3404')
+GO
+INSERT INTO [dbo].[ObjectHeatConsumption]
+           ([primarykey]
+           ,[name]
+           ,[date_registration]
+           ,[consumer_id]
+           ,[building_id])
+     VALUES
+           (NEWID()
+           ,'人孛事'
+           ,'18.10.2018'
+           ,'12663B9A-9543-4726-B904-71F70FB4EB8D'
+           ,'04A02EFA-19E6-44E6-8211-28AAC0EC21DF')
+GO
+
+
+
