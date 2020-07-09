542f7706cc8ebd5d14baafa54728dc8a81596269
iskakov <35532454+iskakov@users.noreply.github.com>
2020-07-09 15:05:39 +0500

    Заполнение значениями таблицы Сетевые Районы

---
+++ in_net_area.sql
@@ -0,0 +1,40 @@
+USE [Task3]
+GO
+
+INSERT INTO [dbo].[NetworkArea]
+           ([primarykey]
+           ,[name])
+     VALUES
+           (NEWID(),
+           'Район №1')
+GO
+INSERT INTO [dbo].[NetworkArea]
+           ([primarykey]
+           ,[name])
+     VALUES
+           (NEWID(),
+           'Район №2')
+GO
+INSERT INTO [dbo].[NetworkArea]
+           ([primarykey]
+           ,[name])
+     VALUES
+           (NEWID(),
+           'Район №3')
+GO
+INSERT INTO [dbo].[NetworkArea]
+           ([primarykey]
+           ,[name])
+     VALUES
+           (NEWID(),
+           'Район №4')
+GO
+INSERT INTO [dbo].[NetworkArea]
+           ([primarykey]
+           ,[name])
+     VALUES
+           (NEWID(),
+           'Район №5')
+GO
+
+
