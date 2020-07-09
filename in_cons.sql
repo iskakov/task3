91924beae57084f2dcf30876e8d1621e110e4ecb
iskakov <35532454+iskakov@users.noreply.github.com>
2020-07-09 15:05:17 +0500

    Заполнение значениями таблицы Потребители

---
+++ in_cons.sql
@@ -0,0 +1,39 @@
+USE [Task3]
+GO
+
+INSERT INTO [dbo].[Consumer]
+           ([primarykey]
+           ,[personal_account])
+     VALUES
+           (NEWID()
+           ,'56347278')
+GO
+INSERT INTO [dbo].[Consumer]
+           ([primarykey]
+           ,[personal_account])
+     VALUES
+           (NEWID()
+           ,'34355335')
+GO
+INSERT INTO [dbo].[Consumer]
+           ([primarykey]
+           ,[personal_account])
+     VALUES
+           (NEWID()
+           ,'45745745')
+GO
+INSERT INTO [dbo].[Consumer]
+           ([primarykey]
+           ,[personal_account])
+     VALUES
+           (NEWID()
+           ,'38732456')
+GO
+INSERT INTO [dbo].[Consumer]
+           ([primarykey]
+           ,[personal_account])
+     VALUES
+           (NEWID()
+           ,'84534683')
+GO
+
