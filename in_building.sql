4666584297e77a2b167dcb7bab29b0d8aece16d7
iskakov <35532454+iskakov@users.noreply.github.com>
2020-07-09 15:04:28 +0500

    Заполнение значениями таблицы Зданий

---
+++ in_building.sql
@@ -0,0 +1,152 @@
+USE [Task3]
+GO
+
+INSERT INTO [dbo].[Building]
+           ([primarykey]
+           ,[city]
+           ,[street]
+           ,[number]
+           ,[network_area_id])
+     VALUES
+           (NEWID()
+           ,'Пермь'
+           ,'Пушкина'
+           ,1
+           ,'E1BBD31B-D895-4039-8F25-0754D12BF040')
+GO
+INSERT INTO [dbo].[Building]
+           ([primarykey]
+           ,[city]
+           ,[street]
+           ,[number]
+           ,[network_area_id])
+     VALUES
+           (NEWID()
+           ,'Пермь'
+           ,'Пушкина'
+           ,2
+           ,'E1BBD31B-D895-4039-8F25-0754D12BF040')
+GO
+INSERT INTO [dbo].[Building]
+           ([primarykey]
+           ,[city]
+           ,[street]
+           ,[number]
+           ,[network_area_id])
+     VALUES
+           (NEWID()
+           ,'Пермь'
+           ,'Пушкина'
+           ,3
+           ,'E1BBD31B-D895-4039-8F25-0754D12BF040')
+GO
+INSERT INTO [dbo].[Building]
+           ([primarykey]
+           ,[city]
+           ,[street]
+           ,[number]
+           ,[network_area_id])
+     VALUES
+           (NEWID()
+           ,'Пермь'
+           ,'Ленина'
+           ,1
+           ,'E1BBD31B-D895-4039-8F25-0754D12BF040')
+GO
+INSERT INTO [dbo].[Building]
+           ([primarykey]
+           ,[city]
+           ,[street]
+           ,[number]
+           ,[network_area_id])
+     VALUES
+           (NEWID()
+           ,'Пермь'
+           ,'Ленина'
+           ,2
+           ,'E1BBD31B-D895-4039-8F25-0754D12BF040')
+GO
+
+INSERT INTO [dbo].[Building]
+           ([primarykey]
+           ,[city]
+           ,[street]
+           ,[number]
+           ,[network_area_id])
+     VALUES
+           (NEWID()
+           ,'Пермь'
+           ,'Ленина'
+           ,5
+           ,'85E258F4-6201-474E-8036-52DF48D18553')
+GO
+INSERT INTO [dbo].[Building]
+           ([primarykey]
+           ,[city]
+           ,[street]
+           ,[number]
+           ,[network_area_id])
+     VALUES
+           (NEWID()
+           ,'Пермь'
+           ,'Ленина'
+           ,6
+           ,'85E258F4-6201-474E-8036-52DF48D18553')
+GO
+INSERT INTO [dbo].[Building]
+           ([primarykey]
+           ,[city]
+           ,[street]
+           ,[number]
+           ,[network_area_id])
+     VALUES
+           (NEWID()
+           ,'Пермь'
+           ,'Ленина'
+           ,7
+           ,'85E258F4-6201-474E-8036-52DF48D18553')
+GO
+
+INSERT INTO [dbo].[Building]
+           ([primarykey]
+           ,[city]
+           ,[street]
+           ,[number]
+           ,[network_area_id])
+     VALUES
+           (NEWID()
+           ,'Пермь'
+           ,'Мира'
+           ,8
+           ,'0C780487-5C03-456E-A981-73A54516A6D1')
+GO
+
+INSERT INTO [dbo].[Building]
+           ([primarykey]
+           ,[city]
+           ,[street]
+           ,[number]
+           ,[network_area_id])
+     VALUES
+           (NEWID()
+           ,'Пермь'
+           ,'Мира'
+           ,9
+           ,'0C780487-5C03-456E-A981-73A54516A6D1')
+GO
+
+INSERT INTO [dbo].[Building]
+           ([primarykey]
+           ,[city]
+           ,[street]
+           ,[number]
+           ,[network_area_id])
+     VALUES
+           (NEWID()
+           ,'Пермь'
+           ,'Мира'
+           ,10
+           ,'0C780487-5C03-456E-A981-73A54516A6D1')
+GO
+
+
