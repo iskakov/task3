1cbfbc88db0bcd14280055bd045a922516fc5d2c
iskakov <35532454+iskakov@users.noreply.github.com>
2020-07-17 16:16:18 +0500

    Update task3_script.sql

--- task3_script.sql
+++ task3_script.sql
@@ -1,6 +1,11 @@
-SELECT Consumer.primarykey, Consumer.personal_account, Count(NetworkSection2ObjectHeatConsum.network_section_id)
+SELECT Consumer.primarykey, Consumer.personal_account, Count(NetworkSection2ObjectHeatConsum.network_section_id) as u
 FROM Consumer left JOIN ObjectHeatConsumption ON Consumer.primarykey = ObjectHeatConsumption.consumer_id
 		LEFT JOIN NetworkSection2ObjectHeatConsum ON ObjectHeatConsumption.primarykey = NetworkSection2ObjectHeatConsum.object_heat_consum_id
 WHERE NetworkSection2ObjectHeatConsum.is_inside = 1
-GROup BY Consumer.primarykey, Consumer.personal_account
-ORDER BY Count(NetworkSection2ObjectHeatConsum.network_section_id) DESC
+Group by Consumer.primarykey, Consumer.personal_account
+
+HAVING Count(NetworkSection2ObjectHeatConsum.network_section_id) =  (SELECT MAX(q1.u) FROM (SELECT Consumer.primarykey, Count(NetworkSection2ObjectHeatConsum.network_section_id) as u
+FROM Consumer left JOIN ObjectHeatConsumption ON Consumer.primarykey = ObjectHeatConsumption.consumer_id
+		LEFT JOIN NetworkSection2ObjectHeatConsum ON ObjectHeatConsumption.primarykey = NetworkSection2ObjectHeatConsum.object_heat_consum_id
+WHERE NetworkSection2ObjectHeatConsum.is_inside = 1
+Group by Consumer.primarykey) as q1)
