60fa259f0bdd342b12b26040ac13922ce2f729a8
iskakov <35532454+iskakov@users.noreply.github.com>
2020-07-17 16:12:17 +0500

    Update in_cons.sql

--- in_cons.sql
+++ in_cons.sql
@@ -5,35 +5,35 @@ INSERT INTO [dbo].[Consumer]
            ([primarykey]
            ,[personal_account])
      VALUES
-           (NEWID()
+           ('70a2dfa4-0367-42cf-a7a2-1410bf231a92'
            ,'56347278')
 GO
 INSERT INTO [dbo].[Consumer]
            ([primarykey]
            ,[personal_account])
      VALUES
-           (NEWID()
+           ('d46ac640-2665-44ce-bf01-0856995f73f2'
            ,'34355335')
 GO
 INSERT INTO [dbo].[Consumer]
            ([primarykey]
            ,[personal_account])
      VALUES
-           (NEWID()
+           ('e99f5745-d1a4-4b5a-9bd4-f6da4b67d487'
            ,'45745745')
 GO
 INSERT INTO [dbo].[Consumer]
            ([primarykey]
            ,[personal_account])
      VALUES
-           (NEWID()
+           ('da34972f-733b-4b2d-9e5e-0aad19803b62'
            ,'38732456')
 GO
 INSERT INTO [dbo].[Consumer]
            ([primarykey]
            ,[personal_account])
      VALUES
-           (NEWID()
+           ('a5e1c914-f0d6-488d-a597-80b97250173c'
            ,'84534683')
 GO
 
